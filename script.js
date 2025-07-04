200c6d575bf5eec91d0971e4087737be20cebb8a
ABDULLAH-prog-cloud <abdullahshaid38@gmail.com>
2025-07-04 08:38:43 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("menu-toggle").addEventListener("click", () => {
-  const sidebar = document.getElementById("sidebar");
-  if (sidebar.style.left === "0px") {
-    sidebar.style.left = "-200px";
-  } else {
-    sidebar.style.left = "0px";
-  }
-});
